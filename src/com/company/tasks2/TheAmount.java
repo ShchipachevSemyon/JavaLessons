ed95dd9d4102ba28ff076469df0c7bd0be6fc1eb
Semyon Shchipachev <saiman.aza@mail.ru>
2022-05-20 16:33:35 +0500

    Correction

--- src/com/company/tasks2/TheAmount.java
+++ src/com/company/tasks2/TheAmount.java
@@ -1,73 +1,27 @@
 package com.company.tasks2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class TheAmount {
 
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
 
-        int step = 0;
-        List <Character> firstBox = new ArrayList<>();
-        firstBox.add('A');
-        firstBox.add('P');
-        firstBox.add('O');
-        firstBox.add('R');
-
-        List <Character> secondBox= new ArrayList<>();
-        secondBox.add('B');
-        secondBox.add('M');
-        secondBox.add('S');
-
-        List <Character> thirdBox = new ArrayList<>();
-        thirdBox.add('D');
-        thirdBox.add('G');
-        thirdBox.add('J');
-        thirdBox.add('K');
-        thirdBox.add('T');
-        thirdBox.add('W');
-
         int n = in.nextInt();
-        List<String> names = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            names.add(in.nextLine());
-        }
-        names.remove(0);
-        for (int i = 0; i < names.size(); i++) {
-            if (step == 0) {
-                if (firstBox.contains(names.get(i).charAt(0))) {
-                } else if (secondBox.contains(names.get(i).charAt(0))) {
-                    step++;
-                } else if (thirdBox.contains(names.get(i).charAt(0))) {
-                    step += 2;
-                }
-            } else if (firstBox.contains(names.get(i - 1).charAt(0))) {
-                if (firstBox.contains(names.get(i).charAt(0))) {
-                } else if (secondBox.contains(names.get(i).charAt(0))) {
-                    step++;
-                } else if (thirdBox.contains(names.get(i).charAt(0))) {
-                    step += 2;
-                }
-            } else if (secondBox.contains(names.get(i - 1).charAt(0))) {
-                if (firstBox.contains(names.get(i).charAt(0))) {
-                    step++;
-                } else if (secondBox.contains(names.get(i).charAt(0))) {
-
-                } else if (thirdBox.contains(names.get(i).charAt(0))) {
-                    step++;
-                }
-            } else if (thirdBox.contains(names.get(i - 1).charAt(0))) {
-                if (firstBox.contains(names.get(i).charAt(0))) {
-                    step += 2;
-                } else if (secondBox.contains(names.get(i).charAt(0))) {
-                    step++;
-                } else if (thirdBox.contains(names.get(i).charAt(0))) {
-                }
+        int number = 0;
+        if (n == 0) {
+            System.out.println(1);
+        } else if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                number += i;
             }
+            System.out.println(number);
+        } else {
+            for (int i = 1; i >= n; i--) {
+                number += i;
+            }
+            System.out.println(number);
         }
-        System.out.println(step);
     }
 }
+
