f456091d5f53217fd75de331c38c119696f6e69f
Semyon Shchipachev <saiman.aza@mail.ru>
2022-10-12 18:27:19 +0500

    Приборка

--- test_rest_servise/src/main/java/com/example/test_rest_service/servise/MyModifyService.java
+++
@@ -1,9 +0,0 @@
-package com.example.test_rest_service.servise;
-
-import com.example.test_rest_service.model.Response;
-
-public interface MyModifyService {
-
-    Response modify(Response response);
-
-}
